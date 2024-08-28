149f69399465b17ed9aa7dcb9762c359467218a7
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-08-28 14:49:05 +0200

    COBOL: ADD Leap shortest text solution
    I wanted to see if this works, and it does, but I don't really like it

--- cobol/leap/src/leap.cob
+++ cobol/leap/src/leap.cob
@@ -4,31 +4,15 @@
        WORKING-STORAGE SECTION.
        01 WS-RESULT PIC 9.
        01 WS-YEAR PIC 9(4).
-       01 DIVIDING.
-           05 QUOTIENT PIC 9.
-           05 REM PIC 9(3).
-           05 DIVIDER PIC 9(3).
-      * Quotient doesn't matter here, but is necessary for DIVIDE
-      * REM sizing takes the MOST SIGNIFICANT NUMBER
-      *    (e.g. REMAINDER = 60, REM = PIC 9 => REM = 6)
-      *    So REM must be at least 3 digits to properly fit WS-YEAR/4
+       01 DIVIDER PIC 9(3).
        PROCEDURE DIVISION.
        LEAP.
-      * Enter solution here
-           DIVIDE WS-YEAR BY 100 GIVING QUOTIENT REMAINDER REM.
-           IF REM > 0 THEN
-              MOVE 4 TO DIVIDER
+           IF FUNCTION MOD(WS-YEAR, 400) = 0 OR
+              (FUNCTION MOD(WS-YEAR, 100) NOT = 0 AND
+                 FUNCTION MOD(WS-YEAR, 4) = 0) THEN
+              MOVE 1 TO WS-RESULT 
            ELSE
-              MOVE 400 TO DIVIDER
+              MOVE 0 TO WS-RESULT
            END-IF.
-           PERFORM IS-LEAP.
        LEAP-EXIT.
            GOBACK.
-
-       IS-LEAP.
-           DIVIDE WS-YEAR BY DIVIDER GIVING QUOTIENT REMAINDER REM.
-           IF REM > 0 THEN
-              MOVE 0 TO WS-RESULT
-           ELSE
-              MOVE 1 TO WS-RESULT
-           END-IF.
