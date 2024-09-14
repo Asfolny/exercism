96c1c83fcb1f90ae1b24e16ec66907188170c1c4
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-09-14 22:57:32 +0200

    Add goroutine

--- go/parallel-letter-frequency/parallel_letter_frequency.go
+++ go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,5 +1,6 @@
 package letter
 
+import "sync"
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
@@ -17,10 +18,31 @@ func Frequency(text string) FreqMap {
 // by making use of concurrency.
 func ConcurrentFrequency(texts []string) FreqMap {
 	m := FreqMap{}
+    ch := make(chan FreqMap, len(texts))
+    var wg sync.WaitGroup
+
     for _, text := range texts {
-        for _, r := range text {
-            m[r]++
+        wg.Add(1)
+        go GoFrequency(text, ch, &wg)
+    }
+    
+    wg.Wait()
+    close(ch)
+
+    for freq := range ch {
+        for k, v := range freq {
+            m[k] += v
         }
     }
+
     return m
+}
+
+func GoFrequency(text string, ch chan FreqMap, wg *sync.WaitGroup) {
+    defer wg.Done()
+    frequencies := FreqMap{}
+	for _, r := range text {
+		frequencies[r]++
+	}
+    ch <- frequencies
 }
