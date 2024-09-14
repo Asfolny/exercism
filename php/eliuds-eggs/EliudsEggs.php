e8e0e3d08a0e5b6fe6dfea685acd197316fce935
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-09-14 11:54:32 +0200

    PHP: ADD Eliud's Eggs solution

---
+++ php/eliuds-eggs/EliudsEggs.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+class EliudsEggs
+{
+    public function eggCount(int $displayValue): int
+    {
+        if ($displayValue === 0) {
+            return 0;
+        }
+
+        return ($displayValue & 1) + $this->eggCount($displayValue >> 1);
+    }
+}
