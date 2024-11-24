9fdbb93efe23e38ec6e54e56b161e70fd3dd16d6
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-11-24 19:53:45 +0100

    PYTHON: ADD Raindrops solution

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,14 @@
+def convert(number):
+    raindrops = ""
+
+    if number % 3 == 0:
+        raindrops += "Pling"
+    if number % 5 == 0:
+        raindrops += "Plang"
+    if number % 7 == 0:
+        raindrops += "Plong"
+    
+    if raindrops == "":
+        return f"{number}"
+
+    return raindrops
