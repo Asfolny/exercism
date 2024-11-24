677faa7427c365d4ee1421eb8b374b686f19aa6d
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-11-24 20:42:47 +0100

    ELIXIR: ADD Kitchen Calculator solution

---
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -0,0 +1,20 @@
+defmodule KitchenCalculator do
+  def get_volume(volume_pair), do: elem(volume_pair, 1)
+
+  def to_milliliter({:cup, volume}), do: {:milliliter, volume * 240}
+  def to_milliliter({:fluid_ounce, volume}), do: {:milliliter, volume * 30}
+  def to_milliliter({:teaspoon, volume}), do: {:milliliter, volume * 5}
+  def to_milliliter({:tablespoon, volume}), do: {:milliliter, volume * 15}
+  def to_milliliter({:milliliter, volume}), do: {:milliliter, volume}
+
+  def from_milliliter({_, volume}, :cup = unit), do: {unit, volume / 240}
+  def from_milliliter({_, volume}, :fluid_ounce = unit), do: {unit, volume / 30}
+  def from_milliliter({_, volume}, :teaspoon = unit), do: {unit, volume / 5}
+  def from_milliliter({_, volume}, :tablespoon = unit), do: {unit, volume / 15}
+  def from_milliliter({_, volume}, :milliliter = unit), do: {unit, volume}
+  
+  def convert(volume_pair, unit) do
+    milliliter_pair = to_milliliter(volume_pair)
+    from_milliliter(milliliter_pair, unit)
+  end
+end
