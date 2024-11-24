bb43e265ae3fbc3ef162aa9ece425da60fc18f88
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-11-24 20:37:32 +0100

    ELIXIR: ADD Lasagna solution

---
+++ elixir/lasagna/lib/lasagna.ex
@@ -0,0 +1,15 @@
+defmodule Lasagna do
+  def expected_minutes_in_oven(), do: 40
+
+  def remaining_minutes_in_oven(time_in) do
+    expected_minutes_in_oven() - time_in
+  end
+
+  def preparation_time_in_minutes(layers), do: layers * 2
+
+  def total_time_in_minutes(layers, time_in) do
+    preparation_time_in_minutes(layers) + time_in
+  end
+  
+  def alarm(), do: "Ding!"
+end
