072f327a52669aefef1bb127c2f76d0a1739c5f7
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-12-11 18:22:26 +0100

    JAVASCRIPT: ADD Bird Watcher solution

---
+++ javascript/bird-watcher/bird-watcher.js
@@ -0,0 +1,38 @@
+// @ts-check
+
+/**
+ * Calculates the total bird count.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number} total bird count
+ */
+export function totalBirdCount(birdsPerDay) {
+  return birdsPerDay.reduce((acc, n) => acc + n, 0)
+}
+
+/**
+ * Calculates the total number of birds seen in a specific week.
+ *
+ * @param {number[]} birdsPerDay
+ * @param {number} week
+ * @returns {number} birds counted in the given week
+ */
+export function birdsInWeek(birdsPerDay, week) {
+  const offset = (week-1) * 7
+  return totalBirdCount(birdsPerDay.slice(offset, offset+7))
+}
+
+/**
+ * Fixes the counting mistake by increasing the bird count
+ * by one for every second day.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number[]} corrected bird count data
+ */
+export function fixBirdCountLog(birdsPerDay) {
+  for (let i = 0; i < birdsPerDay.length; i += 2) {
+    birdsPerDay[i]++
+  }
+
+  return birdsPerDay
+}
