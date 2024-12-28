ef7e367eba14f68dea3f5034aeffec65324f8a9a
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-12-28 13:28:05 +0100

    CLOJURE: ADD Cars, Assemble! solution

---
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -0,0 +1,21 @@
+(ns cars-assemble)
+
+(defn production-rate
+  "Returns the assembly line's production rate per hour,
+   taking into account its success rate"
+  [speed]
+  (let 
+    [modifier (cond 
+                (< speed 1) 0
+                (<= speed 4) 1
+                (<= speed 8) 0.9
+                (= speed 9) 0.8
+                :else 0.77)]
+    (* speed 221 modifier))
+  )
+
+(defn working-items
+  "Calculates how many working cars are produced per minute"
+  [speed]
+  (int (Math/floor (/ (production-rate speed) 60)))
+  )
