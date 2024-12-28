cdb4d1754e3b65b7a5560dd9639b9e590fad5db4
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-12-28 21:44:51 +0100

    F#: ADD Valentine's Day solution

---
+++ fsharp/valentines-day/ValentinesDay.fs
@@ -0,0 +1,35 @@
+module ValentinesDay
+
+type Approval =
+    | Yes
+    | No
+    | Maybe
+
+type Cuisine =
+    | Korean
+    | Turkish
+
+type Genre =
+    | Crime
+    | Horror
+    | Romance
+    | Thriller
+
+type Activity =
+    | BoardGame
+    | Chill
+    | Movie of Genre
+    | Restaurant of Cuisine
+    | Walk of int
+
+let rateActivity (activity: Activity): Approval =
+    match activity with
+    | BoardGame -> No
+    | Chill -> No
+    | Movie genre when genre = Romance -> Yes
+    | Movie _ -> No
+    | Restaurant cuisine when cuisine = Korean -> Yes
+    | Restaurant cuisine when cuisine = Turkish -> Maybe
+    | Walk km when km < 3 -> Yes
+    | Walk km when km < 5 -> Maybe
+    | _ -> No
