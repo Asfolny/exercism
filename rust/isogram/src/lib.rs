165da73bd4855a9d2a545570eb538cbf733e6292
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-12-30 12:35:57 +0100

    RUST: ADD Isogram solution

---
+++ rust/isogram/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn check(candidate: &str) -> bool {
+    let mut seen = Vec::new();
+
+    for c in candidate.to_lowercase().chars() {
+        if (c.is_alphabetic() && seen.contains(&c)) {
+            return false;
+        }
+
+        seen.push(c);
+    }
+
+    return true;
+}
