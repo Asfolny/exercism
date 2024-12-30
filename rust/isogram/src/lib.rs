482c4ef1319f1360bec674c61a122a1b2c4f6037
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-12-30 12:36:53 +0100

    --amend

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -2,12 +2,12 @@ pub fn check(candidate: &str) -> bool {
     let mut seen = Vec::new();
 
     for c in candidate.to_lowercase().chars() {
-        if (c.is_alphabetic() && seen.contains(&c)) {
+        if c.is_alphabetic() && seen.contains(&c) {
             return false;
         }
 
         seen.push(c);
     }
 
-    return true;
+    true
 }
