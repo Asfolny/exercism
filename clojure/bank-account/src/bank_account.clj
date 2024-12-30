9b766bb5623909470e6b1759618c0b5fb1fabe84
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-12-30 14:44:26 +0100

    CLOJURE: ADD Bank Account solution

---
+++ clojure/bank-account/src/bank_account.clj
@@ -0,0 +1,22 @@
+(ns bank-account)
+
+(defn open-account
+  "Opens given account. This is the first operation starting the account life."
+  []
+  (atom 0))
+
+(defn close-account
+  "Closes the account. No further operations are allowed on given account,
+  that is `nil` is returned by any other operation that will use closed account."
+  [account]
+  (reset! account nil))
+
+(defn get-balance
+  "Returns current balance of the account."
+  [account]
+  @account)
+
+(defn update-balance
+  "Updates balance of given account adding the amount."
+  [account amount]
+  (swap! account + amount))
